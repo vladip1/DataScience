ead5d23862853e5d99cdbd0d98f456180e109940
“Vladi <polonskyv@yahoo.com>
2019-11-26 00:27:35 +0200

    homewrok

--- R/R-final.R
+++ R/R-final.R
@@ -10,31 +10,67 @@ library(DBI)
 con <- dbConnect(odbc::odbc(), .connection_string = "DSN=COLLEGE;Trusted_Connection=yes;", timeout = 10)
 
 ## Get the whole table:
-df <- dbReadTable(con, "Classrooms")
+Classrooms <- dbReadTable(con, "Classrooms")
+Courses <- dbReadTable(con, "Courses")
+Students <- dbReadTable(con, "Students")
+Teachers <- dbReadTable(con, "Teachers")
+Departments <- dbReadTable(con, "Departments")
 
 library(dplyr)
+
 
 ## Questions
 
 ##############
-## Q1. Count the number of students on each department¶
+## Q1. Count the number of students on each department
 ##############
 
-as_tibble(df) %>%
+
+df<-dplyr::left_join(Classrooms, Courses, by = "CourseId")
+df<-rename(df, DepartmentId = DepartmentID)
+df<-dplyr::left_join(df, Departments, by = "DepartmentId")
+
+res<-df %>% select(StudentId, DepartmentName) %>% distinct %>%
+  group_by(DepartmentName) %>%
   summarise(count<-n())
 
-
+print(res)
 
 ##############
 ## Q2. How many students have each course of the English department and the 
 ##     total number of students in the department?
 ##############
+df<-dplyr::left_join(Classrooms, Courses, by = "CourseId")
+df<-rename(df, DepartmentId = DepartmentID)
+df<-dplyr::left_join(df, Departments, by = "DepartmentId")
 
+#students in each course
+res<-df %>% filter(DepartmentName == 'English') %>% select(StudentId, CourseName) %>%
+  group_by(CourseName) %>% summarise(count=n())
+
+print(res)
+
+#students in English department
+res_all<-df %>% filter(DepartmentName == 'English') %>% select(StudentId) %>%
+  distinct %>% summarise(count=n())
+
+print(res_all)
 
 ##############
 ## Q3. How many small (<22 students) and large (22+ students) classrooms are 
 ##     needed for the Science department?
 ##############
+df<-dplyr::left_join(Classrooms, Courses, by = "CourseId")
+df<-rename(df, DepartmentId = DepartmentID)
+df<-dplyr::left_join(df, Departments, by = "DepartmentId")
+
+res<-df %>% filter(DepartmentName == 'Science') %>% select(StudentId, CourseName) %>%
+  group_by(CourseName) %>% summarise(count=n()) %>%
+  mutate(size = ifelse(count >= 22,'BIG','SMALL')) %>% select(size)
+
+res<-table(res)
+
+print(res)
 
 
 ##############
@@ -42,10 +78,21 @@ as_tibble(df) %>%
 ##     College. Justify if the argument is correct
 ##############
 
+table(Students$Gender)
 
 ##############
 ## Q5. For which courses the percentage of male/female students is over 70%?
 ##############
+df<-dplyr::left_join(Classrooms, Courses, by = "CourseId")
+df<-dplyr::left_join(df, Students, by = "StudentId")
+
+res<-df %>% select(CourseName, StudentId, Gender) %>%
+  mutate(male = ifelse(Gender == 'M',1,0)) %>% 
+  group_by(CourseName) %>% 
+  
+  summarise(male_percentage = sum(male)/count())
+
+
 
 
 ##############
