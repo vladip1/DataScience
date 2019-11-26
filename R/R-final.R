5acc385c85b100816312ed0e2e44c37a8d5209a3
“Vladi <polonskyv@yahoo.com>
2019-11-26 23:42:08 +0200

    R exercise

--- R/R-final.R
+++ R/R-final.R
@@ -89,25 +89,68 @@ df<-dplyr::left_join(df, Students, by = "StudentId")
 res<-df %>% select(CourseName, StudentId, Gender) %>%
   mutate(male = ifelse(Gender == 'M',1,0)) %>% 
   group_by(CourseName) %>% 
-  
-  summarise(male_percentage = sum(male)/count())
+  summarise(male_percentage = sum(male)/n() * 100,
+         female_percenatge = 100 - male_percentage) %>% filter(male_percentage > 70 | female_percenatge > 70)
 
-
+print(res)
 
 
 ##############
 ## Q6. For each department, how many students passed with a grades over 80?
 ##############
 
+df<-dplyr::left_join(Classrooms, Courses, by = "CourseId")
+df<-rename(df, DepartmentId = DepartmentID)
+df<-dplyr::left_join(df, Departments, by = "DepartmentId")
+
+#Assumption - graduation degree = average degree of all the courses belonging to that department
+#same as in SQL exercize
+res<-df %>%  
+  group_by(StudentId,DepartmentName) %>% mutate(avg=mean(degree)) %>% 
+  distinct(StudentId, DepartmentName, avg) %>% 
+  group_by(DepartmentName) %>% 
+  filter(avg>80) %>%
+  summarise(count=n())
+
+print(res)
 
 ##############
 ## Q7. For each department, how many students passed with a grades under 60?
 ##############
+df<-dplyr::left_join(Classrooms, Courses, by = "CourseId")
+df<-rename(df, DepartmentId = DepartmentID)
+df<-dplyr::left_join(df, Departments, by = "DepartmentId")
+
+#Assumption - graduation degree = average degree of all the courses belonging to that department
+#same as in SQL exercize
+res<-df %>%  
+  group_by(StudentId,DepartmentName) %>% mutate(avg=mean(degree)) %>% 
+  distinct(StudentId, DepartmentName, avg) %>% 
+  group_by(DepartmentName) %>% 
+  filter(avg<60) %>%
+  summarise(count=n())
+
+print(res)
 
 
 ##############
 ## Q8. Rate the teachers by their average student's grades (in descending order).
 ##############
+df<-dplyr::left_join(Classrooms, Courses, by = "CourseId")
+df<-rename(df, DepartmentId = DepartmentID)
+
+res<-df %>%  
+  group_by(TeacherId) %>% mutate(students_avg_grade=mean(degree)) %>% 
+  distinct(TeacherId, students_avg_grade) %>% 
+  arrange(desc(students_avg_grade))
+
+
+res<-dplyr::left_join(res, Teachers, by = "TeacherId")
+
+  
+print(res)
+
+
 
 
 ##############
@@ -115,12 +158,50 @@ res<-df %>% select(CourseName, StudentId, Gender) %>%
 ##     the teacher in each course, and the number of students enrolled in the course 
 ##     (for each course, department and teacher show the names).
 ##############
+df<-dplyr::left_join(Classrooms, Courses, by = "CourseId")
+df<-rename(df, DepartmentId = DepartmentID)
+df<-dplyr::left_join(df, Departments, by = "DepartmentId")
 
+res<-df %>%
+  group_by(CourseName, DepartmentName, TeacherId) %>%
+  summarise(students = n())
+
+res<-dplyr::left_join(res, Teachers, by = "TeacherId") 
+
+res <- res %>% select(-c(TeacherId, Gender))
+
+print(res)
 
 ##############
 ## Q10. Create a dataframe showing the students, the number of courses they take, 
 ##      the average of the grades per class, and their overall average (for each student 
 ##      show the student name).
 ##############
+
+df<-dplyr::left_join(Classrooms, Courses, by = "CourseId")
+df<-rename(df, DepartmentId = DepartmentID)
+df<-dplyr::left_join(df, Departments, by = "DepartmentId")
+
+
+print(df)
+
+res<- df %>%
+  group_by(StudentId,DepartmentName) %>% 
+  mutate(avg=mean(degree)) %>%
+  group_by(StudentId) %>% 
+  mutate(total_avg=mean(degree), courses=n()) %>%
+  select(StudentId,DepartmentName, avg, total_avg, courses) %>%
+  distinct() %>%
+  arrange(desc(StudentId)) 
+
+
+library(tidyr)
+
+res<-pivot_wider(res, names_from = DepartmentName, values_from = avg)
+res<-dplyr::left_join(res, Students, by = "StudentId")
+res<-res%>%select(StudentId, FirstName, LastName, courses, total_avg, Science, Arts, Sport, English) %>%
+  arrange(FirstName, LastName)
+
+print(res)
 
 
